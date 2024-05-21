dc7706a2d34dd6a017b762d0851114d2ba2ab60c
SantiagoVanegas <luis.vanegas01@uceva.edu.co>
2024-05-20 20:24:10 -0500

    PrimerCommit

---
+++ app/routes.py
@@ -0,0 +1,16 @@
+from flask import jsonify, request
+from app import db, create_app
+from app.models import Item
+
+@app.route('/items', methods=['GET'])
+def get_items():
+    items = Item.query.all()
+    return jsonify([item.to_dict() for item in items])
+
+@app.route('/items', methods=['POST'])
+def create_item():
+    data = request.get_json()
+    new_item = Item(name=data['name'])
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify(new_item.to_dict()), 201
